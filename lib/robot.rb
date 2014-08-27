d7b5cf4e68cb263ed4cddbae3e84c8c1fa043640
Gilang Mahardhika <gilangmahardhika@gmail.com>
2014-08-27 13:31:24 +0700

    change to standard input

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,2 +1,31 @@
 class Robot
+  COMMANDS = ["MOVE", "LEFT", "RIGHT", "REPORT"]
+  
+  def self.command
+    print "Write your command\n"
+    input = gets.strip
+    if self.validates_input?(input)
+      self.execute_command(input)
+    else
+      abort("Invalid Input")
+    end
+  end
+  
+  protected
+  def self.execute_command(command)
+    if /PLACE,[0-9],[0-9],(NORTH|EAST|WEST|SOUTH)/.match(command)
+      @x = command.split(',')[1]
+      @y = command.split(',')[2]
+      @f = command.split(',')[3]
+      self.command
+    end
+  end
+
+  def self.validates_input?(command)
+    COMMANDS.include?(command) ||  (/PLACE,[0-9],[0-9],(NORTH|EAST|WEST|SOUTH)/.match(command))
+  end 
+
+  def set_output(command)
+    print command
+  end
 end
---
+++ spec/robot_spec.rb
@@ -0,0 +1,6 @@
+require 'robot'
+RSpec.describe Robot do
+  it "only can the valid commands" do
+    
+  end
+end
